5cc2219d9362a1ee3e703f2c0ad307e5fbcdc43c
YeongKyunJeong <49927788+YeongKyunJeong@users.noreply.github.com>
2019-11-04 15:46:52 +0900

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,3 @@
-
 import os
 import glob
 from astropy.io import fits
@@ -8,9 +7,22 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 import matplotlib.colors as colors
 
+import astropy.units as u
+from astropy.nddata import CCDData
+from ccdproc import Combiner
+
 #%%
-namepath = 'C:/Users/jjbyk/AO2/project/2019-10-24'
-fitspath = Path('C:/Users/jjbyk/AO2/project/2019-10-24')
+# 아래에 fits파일이 있는 directory 위치를 적을 것.
+
+#집용
+#namepath = 'C:/Users/jjbyk/AO2/project/2019-10-24'
+#fitspath = Path('C:/Users/jjbyk/AO2/project/2019-10-24')
+
+#학과전산실용
+namepath = '/home/astro_02/AO2019-2/2019-10-24'
+fitspath = Path('/home/astro_02/AO2019-2/2019-10-24')
+
+
 os.chdir(namepath)
 #os.chdir("..") directory 변경
 #allfits = fitspath.glob('*.fit')
@@ -94,15 +106,8 @@ for i in range(len(exptlist)):
     dark = table[((table['OBJECT']=='Calibration') & (table['EXPTIME'] == exptlist[i]))]
     darkdic[exptlist[i]]=dark
 
-#flat의 exptime이 하나일 경우
-#flattab = table[(table['OBJECT'] == 'Flat')]
 
-#이번 경우(expt = 10)만 사용
-flattab = table[((table['OBJECT'] == 'Flat') & (table['EXPTIME'] == 10))]
-
-#30초가 맞는지 10초가 맞는지, 둘 다 쓰는지 몰라 만든 예비
-#flattab10 = table[((table['OBJECT'] == 'Flat') & (table['EXPTIME'] == 10))]
-#flattab30 = table[((table['OBJECT'] == 'Flat') & (table['EXPTIME'] == 30))]
+flattab = table[(table['OBJECT'] == 'Flat')]
 
 """ 30초가 맞는지 10초가 맞는지 상의할 것"""
 
@@ -126,32 +131,75 @@ flat_fname = 'flat.fits'
 
 
 
+#%%
+prepath=Path(fitspath/'Preprocessed')
+if not prepath.exists():
+    prepath.mkdir()
+else:
+    print("Prepath 저장 폴더가 이미 있어서 새로 만들지 않음")
+
+
 
 if os.path.exists(bias_fname):
     mbias = fits.getdata(bias_fname)
 
-"""
-else:
-    mbias = preproc.make_master_bias(biastab, sigma=3, iters=5, min_value=0,
-                                     output = bias_fname)
-"""
- 
+else:   
+    images=[]
+    for i in range(len(biastab)):
+        cc = CCDData(fits.getdata(biastab[i]['FILE']), unit = u.adu)
+        images.append(cc)
+        
+    cc = Combiner(images)
+    mbias = cc.median_combine()
+    cc = fits.getheader(biastab[0]['FILE'])
+    
+    mbias.header = cc
+    mbias.header.add_history(f"{len(biastab)} image(s) median combined bias frames")
+    mbias.write(prepath/bias_fname,overwrite=True)
+
+
+
+
 mdark_fdic={}
 for i in range(len(exptlist)):
     if os.path.exists(dark_fdic[exptlist[i]]):
         mdark_fdic[exptlist[i]] = fits.getdata(dark_fdic[exptlist[i]])
 
-"""        
-    else:
-         mdark_fdic[exptlist[i]] = preproc.make_master_dark(darkdic[exptlist[i]],
-                                     mbias=mbias, sigma=3, iters=5,
-                                     min_value=0, 
-                                     output = dark_fname)
-"""        
-    
+else:
+    for j in exptlist:   
+
+        images=[]
+        for i in range(len(darkdic[j])):
+            cc = CCDData(fits.getdata(darkdic[j][i]['FILE']),unit= u.adu)
+            images.append(cc)
+        cc = Combiner(images)
+        mdark_fdic[j] = cc.median_combine()
+        cc = fits.getheader(darkdic[j][0]['FILE'])
+        
+        mdark_fdic[j].header = cc
+        mdark_fdic[j].header.add_history(f"{len(darkdic[j])} image(s) median combined dark frames")
+        mdark_fdic[j].write(prepath/dark_fdic[j],overwrite=True)    
+
+
+#%%
 
 if os.path.exists(flat_fname):
     mflat = fits.getdata(flat_fname) 
+
+else:
+
+    images=[]
+    for i in range(len(flattab)):
+        cc = CCDData(fits.getdata(flattab[i]['FILE']), unit = u.adu)
+        images.append(cc)
+
+    cc = Combiner(images)
+    mflat = cc.median_combine()
+    cc = fits.getheader(flattab[0]['FILE'])
+
+    mflat.header = cc
+    mflat.header.add_history(f"{len(flattab)} image(s) median combined flat frames")
+    mflat.write(prepath/flat_fname,overwrite=True)
 
 """
 else:
@@ -160,15 +208,57 @@ else:
                                      output = flat_fname)
 """
 #%%
+mflat.header
+#%%
 
 """Object Image Table 나누기"""
 
 
 #%%
-print(darkdic[exptlist[0]])
-print(dark_fdic[exptlist[0]])
-print(mdark_fdic[exptlist[0]])
+test1 = CCDData(np.ones((4,4)),unit=u.adu)
+test2 = CCDData(np.zeros((4,4)),unit=u.adu)
+test3 = CCDData(np.ones((4,4)),unit=u.adu)
+c = Combiner([test1, test2, test3])
+#%%
+ccdavg = c.average_combine()
+ccdmed = c.median_combine()
+print(ccdavg,ccdmed,sep='\n')
+print(type(test1))
+#%%
+fig = plt.figure(figsize=(10,5))
+ax1 = fig.add_subplot(1,2,1)
+ax2 = fig.add_subplot(1,2,2)
+im1 = ax1.imshow(test1)
+im2 = ax2.imshow(test1)
+#%%
+test = []
+for i in range(len(biastab)):
+    cc = CCDData(fits.getdata(biastab[i]['FILE']),unit= u.adu)
+    test.append(cc)
+print(len(test))
+#test1 = CCDData(fits.getdata('Bias-0001.fit'),unit= u.adu)
+#c = Combiner([test1])
+#print(test1.shape,type(test1))
+#%%
+cc = Combiner(test)
 
+#%%
+
+ccdavg = cc.average_combine()
+ccdmed = cc.median_combine()
+print(ccdavg,ccdmed,sep='\n')
+
+
+#%%
+for i in range(500,510):
+
+    testnum=[]
+    for j in range(len(biastab)):
+        testnum.append(float(fits.getdata(f'Bias-000{j+1}.fit')[i][i]))
+    print(np.mean(testnum),np.median(testnum))
+#%%
+for i in range(500,510):
+    print(ccdavg[i][i],ccdmed[i][i])
 #실행용이므로 남겨둘 것
 
 
